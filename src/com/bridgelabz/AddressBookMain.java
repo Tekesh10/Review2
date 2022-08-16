e7344caa6d671c21708bdeb084a6d850d4efe714
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-16 09:35:52 +0530

    Address Book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -16,12 +16,12 @@ public class AddressBookMain {
         contactList.add(contactOne);
         System.out.println(contactOne);
 
-        String name = "Aman Kumar";
-        String age = "26";
-        String phoneNumber = "7560778773";
-        String city = "Mumbai";
-        String state = "Maharastra";
-        Contact contactTwo = new Contact(name, age, phoneNumber, city, state);
+        String name2 = "Amit Kumar";
+        String age2 = "24";
+        String phoneNumber2 = "7569897698";
+        String city2 = "Hydrabad";
+        String state2 = "Telangana";
+        Contact contactTwo = new Contact(name2, age2, phoneNumber2, city2, state2);
         contactList.add(contactTwo);
         System.out.println(contactTwo);
     }
